03520dcab620cfe5414dd5551b8d31744ba0c1a4
Edgar <Edgar@AnotherFoxGuy.com>
2022-07-14 14:15:31 +0000

    test

---
+++ CMake/Dependencies_Conan.cmake
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for OGRE
+#     (Object-oriented Graphics Rendering Engine)
+# For the latest info, see http://www.ogre3d.org/
+#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
+
+#######################################################################
+# Find all necessary and optional OGRE dependencies
+#######################################################################
+
+include(${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)
+conan_define_targets()
+
